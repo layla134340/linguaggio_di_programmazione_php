09921fe8e87ffe3c661fdd861e098ef7a75b826e
Layla <cbdxweed@gmail.com>
2023-10-10 14:57:37 +0200

    ciao che palle

--- basi_php/05_galleria_arte_moderna.php
+++ basi_php/05_galleria_arte_moderna.php
@@ -24,6 +24,7 @@ print_r($opere_array[3]);
    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esercizio Opere</title>
+    <h1>Ciao che palle</h1>
 </head>
 <body>
     
